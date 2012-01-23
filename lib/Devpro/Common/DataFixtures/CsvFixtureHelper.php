44f86981d8be24ae610ffe869ea31fc50b840843
Bertrand THOMAS <bertrand@devpro.fr>
2012-01-23 14:10:53 +0100

    Added unit test for CsvFixtureHelper

--- lib/Devpro/Common/DataFixtures/CsvFixtureHelper.php
+++ lib/Devpro/Common/DataFixtures/CsvFixtureHelper.php
@@ -6,30 +6,28 @@ use Devpro\Common\Exception\DevproUtilitiesBundleException;
 
 /**
  * Csv fixture helper.
- * 
+ *
  * @author Bertrand THOMAS <bertrand@devpro.fr>
  */
 class CsvFixtureHelper
 {
   /**
    * Créer les entités à partir d'un fichier CSV.
-   * 
+   *
    * @param string $model classe de l'entité
    * @param array $fields champs
    * @param string $file_path chemin du fichier
-   * @throws DevproUtilitiesBundleException si le fichier ne peut pas être lu
    * @return array
+   * @throws DevproUtilitiesBundleException si le fichier ne peut pas être lu
+   * @static
    */
   public static function createEntities($model, $fields, $file_path)
   {
     // lecture du fichier texte
-    $lines = file($file_path, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
-    if ($lines === false) {
-      throw new DevproUtilitiesBundleException('File "%s" cannot be read', $file_path);
-    }
-    
+    $lines = static::readFile($file_path);
+
     $entities = array();
-    
+
     // récupération des en-têtes
     $headers = explode(';', $lines[0]);
     array_walk($headers, create_function('&$val', '$val = str_replace(\'_\', \'\', $val);')); // suppression des _
@@ -43,7 +41,24 @@ class CsvFixtureHelper
       }
       $entities[] = $m;
     }
-    
+
     return $entities;
+  }
+
+  /**
+   * Read file and return array of lines.
+   *
+   * @param string $file_path
+   * @return array
+   * @throws DevproUtilitiesBundleException if file cannot be read
+   * @static
+   */
+  protected static function readFile($file_path)
+  {
+    $lines = file($file_path, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
+    if ($lines === false) {
+      throw new DevproUtilitiesBundleException('File "%s" cannot be read', $file_path);
+    }
+    return $lines;
   }
 }
--- tests/Devpro/Tests/Common/DataFixtures/CsvFixtureHelperTest.php
+++ tests/Devpro/Tests/Common/DataFixtures/CsvFixtureHelperTest.php
@@ -2,10 +2,29 @@
 
 namespace Devpro\Tests\Common\DataFixtures;
 
-class ClientTest extends \PHPUnit_Framework_TestCase
+/**
+ * Test CsvFixtureHelper.
+ *
+ * @package    Devpro
+ * @subpackage Tests
+ * @author     Bertrand THOMAS <bertrand@iocean.fr>
+ */
+class CsvFixtureHelperTest extends \PHPUnit_Framework_TestCase
 {
-  public function testDummy()
+  public function testCreateEntitiesCasNominal()
   {
-    #TODO
+    $fixtureHelper = $this->getMockClass('Devpro\Common\DataFixtures\CsvFixtureHelper', array('readFile'));
+    $fixtureHelper::staticExpects($this->any())
+      ->method('readFile')
+      ->with($this->equalTo('tmp/dummy.txt'))
+      ->will($this->returnValue(array('Name;', 'Toto;', 'Titi;')));
+
+    $result = $fixtureHelper::createEntities('Devpro\Tests\Entity\DummyEntity', array('Name'), 'tmp/dummy.txt');
+
+    $this->assertEquals(2,                                 count($result));
+    $this->assertEquals('Devpro\Tests\Entity\DummyEntity', get_class($result[0]));
+    $this->assertEquals('Toto',                            $result[0]->getName());
+    $this->assertEquals('Devpro\Tests\Entity\DummyEntity', get_class($result[1]));
+    $this->assertEquals('Titi',                            $result[1]->getName());
   }
 }
